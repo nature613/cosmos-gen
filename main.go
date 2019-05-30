e53bd333962b64594ffc918a265ca4cc487c8a75
nature613 <chikonakamura613@gmail.com>
2019-05-30 12:46:40 -0700

    Hello World

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello")
+	f, err := os.Create("module.go")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString("Hello World")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
