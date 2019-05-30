bc953a2d232a9548ed1866bff839a07ba3a2fc47
nature613 <chikonakamura613@gmail.com>
2019-05-30 12:54:37 -0700

    Parsing template

--- main.go
+++ main.go
@@ -1,27 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"text/template"
 )
 
+type Module struct {
+	Name string
+}
+
 func main() {
-	fmt.Println("Hello")
-	f, err := os.Create("module.go")
+	m := Module{"Post"}
+
+	// create a new template with data
+	tmpl := template.New("module")
+	tmpl, err := tmpl.Parse("Hello {{.Name}}")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal("Parse:", err)
 		return
 	}
-	l, err := f.WriteString("Hello World")
+
+	// merge template with contents of m
+	err = tmpl.Execute(os.Stdout, m)
 	if err != nil {
-		fmt.Println(err)
-		f.Close()
+		log.Fatal("Execute: ", err)
 		return
 	}
-	fmt.Println(l, "bytes written successfully")
-	err = f.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+
+	// fmt.Println("Hello")
+	// f, err := os.Create("module.go")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// l, err := f.WriteString("Hello World")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	f.Close()
+	// 	return
+	// }
+	// fmt.Println(l, "bytes written successfully")
+	// err = f.Close()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 }
